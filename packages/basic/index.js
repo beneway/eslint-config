b5ec0f276493f0ea563ca5be6ef73c1e69e467cc
elonehoo <elonehoo@gmail.com>
2023-01-06 13:08:28 +0800

    fix: update rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -341,8 +341,8 @@ module.exports = {
     'yml/no-empty-document': 'off',
 
     // benewy
-    '@benewy/eslint-plugin-benewy/if-newline': 'error',
-    '@benewy/eslint-plugin-benewy/import-dedupe': 'error',
-    // 'benewy/prefer-inline-type-import': 'error',
+    // '@benewy/eslint-plugin-benewy/if-newline': 'error',
+    // '@benewy/eslint-plugin-benewy/import-dedupe': 'error',
+    // '@benewy/eslint-plugin-benewy/prefer-inline-type-import': 'error',
   },
 }
