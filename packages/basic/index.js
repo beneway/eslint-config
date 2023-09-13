acfc132b142846ec8b4d1f27326e3a53be883302
elonehoo <hi@elonehoo.me>
2023-09-13 09:41:43 +0800

    fix: disable more jsdocs rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -413,6 +413,9 @@ module.exports = {
     'jsdoc/require-param-description': 'off',
     'jsdoc/require-yields': 'off',
     'jsdoc/tag-lines': 'off',
+    'jsdoc/check-values': 'off',
+    'jsdoc/check-tag-names': 'off',
+    'jsdoc/no-undefined-types': 'off',
 
     // beneway
     'beneway/if-newline': 'error',
